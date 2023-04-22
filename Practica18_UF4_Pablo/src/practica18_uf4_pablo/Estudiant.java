53f639b34968f39030b810a25965d5941ea822aa
Pablo Jiménez <Pablo Jimnez@DESKTOP-UEAH07A>
2023-04-22 14:10:31 +0200

    dioo

---
+++ Practica18_UF4_Pablo/src/practica18_uf4_pablo/Estudiant.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica18_uf4_pablo;
+
+/**
+ *
+ * @author Pablo Jiménez
+ */
+public class Estudiant extends Persona {
+    
+}
---
+++ Practica18_UF4_Pablo/src/practica18_uf4_pablo/Practica18_UF4_Pablo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica18_uf4_pablo;
+
+/**
+ *
+ * @author Pablo Jiménez
+ */
+public class Practica18_UF4_Pablo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
