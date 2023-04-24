44692b01a04c97b953e3ee2d09064a7a722f3a0b
ausias <ausias@hINFO6-5.xiam.cat>
2023-04-24 10:38:27 +0200

    aaaaaaqaa

--- Practica18_UF4_Pablo/src/practica18_uf4_pablo/Practica18_UF4_Pablo.java
+++ Practica18_UF4_Pablo/src/practica18_uf4_pablo/Practica18_UF4_Pablo.java
@@ -19,6 +19,7 @@ public class Practica18_UF4_Pablo {
     private static ArrayList<Docent> docents = new ArrayList<Docent>();
     private static ArrayList<Personal> personal = new ArrayList<Personal>();
     private static ArrayList<Estudiant> estudiants = new ArrayList<Estudiant>();
+    static final int MAX_TUTORS = 2;
     public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -150,9 +151,9 @@ public class Practica18_UF4_Pablo {
         LocalDate dataActual = LocalDate.now();
         anyActual = dataActual.getYear();
         edat = anyActual - any;
-        if (edat < 18) {         
-            String[] mailTutor = new String[2];
-            String[] adrecaTutor = new String[2];
+        if (edat < 18) {
+            String[] mailTutor = new String[MAX_TUTORS];
+            String[] adrecaTutor = new String[MAX_TUTORS];
 
             for (int i = 0; i < mailTutor.length; i++) {
                 System.out.print("Correu del tutor " + i + ": ");
@@ -249,7 +250,7 @@ public class Practica18_UF4_Pablo {
                 if (alumne instanceof AlumneMenor) {
                     String correuTutor[] = ((AlumneMenor) alumne).correuTutor;
                     correu = alumne.getEmailPersonal();
-                    cc = alumne.getEmailCentre() + ", "+ correuTutor[1] + ", " + correuTutor[0];
+                    cc = alumne.getEmailCentre() + ", " + correuTutor[1] + ", " + correuTutor[0];
                     System.out.print("Concepte del mail: ");
                     subject = scan.next();
 
@@ -308,34 +309,33 @@ public class Practica18_UF4_Pablo {
         }
 
         if (encontrado) {
-                if (alumne instanceof AlumneMenor) {
-                    String correuTutor[] = ((AlumneMenor) alumne).correuTutor;
-                    correu = alumne.getEmailCentre();
-                    cc = correuTutor[1] + ", " + correuTutor[0];
-                    
+            if (alumne instanceof AlumneMenor) {
+                String correuTutor[] = ((AlumneMenor) alumne).correuTutor;
+                correu = alumne.getEmailCentre();
+                cc = correuTutor[1] + ", " + correuTutor[0];
 
+                System.out.print("Missatge del mail: ");
+                missatge = scan.next();
+
+            } else if (alumne instanceof AlumneMajor) {
+                boolean autoritzacio = ((AlumneMajor) alumne).isAutoritzacioTutors();
+                if (!autoritzacio) {
+                    correu = alumne.getEmailCentre();
+                    cc = alumne.getEmailPersonal();
+                    System.out.print("Missatge del mail: ");
+                    missatge = scan.nextLine();
+                } else {
+                    correu = alumne.getEmailPersonal();
+                    System.out.print("Introdueix el mail d'un tutor: ");
+                    cc = scan.next();
                     System.out.print("Missatge del mail: ");
                     missatge = scan.next();
-
-                } else if (alumne instanceof AlumneMajor) {
-                    boolean autoritzacio = ((AlumneMajor) alumne).isAutoritzacioTutors();
-                    if (!autoritzacio) {
-                        correu = alumne.getEmailCentre();
-                        cc = alumne.getEmailPersonal();
-                        System.out.print("Missatge del mail: ");
-                        missatge = scan.nextLine();
-                    } else {
-                        correu = alumne.getEmailPersonal();
-                        System.out.print("Introdueix el mail d'un tutor: ");
-                        cc = scan.next();
-                        System.out.print("Missatge del mail: ");
-                        missatge = scan.next();
-                    }
                 }
-                UtilsExamenUF3.EnviarEmail(correu, cc, subject, missatge);
-            } else {
-                System.out.println("No s'ha trobat cap alumne amb aquest DNI");
             }
+            UtilsExamenUF3.EnviarEmail(correu, cc, subject, missatge);
+        } else {
+            System.out.println("No s'ha trobat cap alumne amb aquest DNI");
+        }
     }
 
     private static void mostrarEstadistiques() {
@@ -364,6 +364,101 @@ public class Practica18_UF4_Pablo {
     }
 
     private static void imprimirEtiquetesNadal() {
+        boolean encontrado = false;
+        String nom = "";
+        String adreca = "";
+        int antiguitat;
+
+        System.out.println("Per a qui vols enviar un mail?");
+        int opcio = Utils.LlegirInt("1.Personal \n2.Docent \n3.Estudiant\n");
+
+        System.out.print("Quin DNI: ");
+        String dni = scan.next();
+
+        if (opcio == 1) {
+            Personal p = null;
+            for (int i = 0; i < personal.size(); i++) {
+                if (personal.get(i).getDni().equals(dni)) {
+                    p = personal.get(i);
+                    encontrado = true;
+                }
+            }
+            if (encontrado) {
+                nom = p.getNom();
+                antiguitat = p.getAnyInici();
+                System.out.print("Introdueix una adreça");
+                adreca = scan.next();
+                UtilsExamenUF3.ImprimirEtiqueta(nom, antiguitat, adreca);
+            } else {
+                System.out.print("No s'ha trobat cap empleat de personal amb aquest DNI ");
+            }
+
+        } else if (opcio == 2) {
+            Docent d = null;
+            for (int i = 0; i < docents.size(); i++) {
+                if (docents.get(i).getDni().equals(dni)) {
+                    d = docents.get(i);
+                    encontrado = true;
+                }
+            }
+            if (encontrado) {
+                nom = d.getNom();
+                antiguitat = d.getAnyInici();
+                System.out.print("Introdueix una adreça");
+                adreca = scan.next();
+                UtilsExamenUF3.ImprimirEtiqueta(nom, antiguitat, adreca);
+            } else {
+                System.out.print("No s'ha trobat cap docent amb aquest DNI ");
+            }
+
+        } else {
+            Estudiant alumne = null;
+            for (int i = 0; i < estudiants.size(); i++) {
+                if (estudiants.get(i).getDni().equals(dni)) {
+                    if (alumne instanceof AlumneMenor) {
+                        alumne = estudiants.get(i);
+                    } else {
+                        alumne = estudiants.get(i);
+                    }
+                    encontrado = true;
+                }
+            }
+            if (encontrado) {
+                if (alumne instanceof AlumneMenor) {
+                    String adrecaTutor[] = ((AlumneMenor) alumne).adrecapostalTutor;
+                    if (!adrecaTutor[0].equals(adrecaTutor[1])) {
+                        /*Si les adreces entre els dos tutors son diferents, primer enviara la etiqueta
+                        al primer tutor y després al segon
+                        */ 
+                        nom = alumne.getNom();
+                        antiguitat = alumne.getAnyMatricula();
+                        adreca = adrecaTutor[0];
+                        UtilsExamenUF3.ImprimirEtiqueta(nom, antiguitat, adreca);
+                        
+                        nom = alumne.getNom();
+                        antiguitat = alumne.getAnyMatricula();
+                        adreca = adrecaTutor[1];
+                        UtilsExamenUF3.ImprimirEtiqueta(nom, antiguitat, adreca);
+                    }else{
+                        nom = alumne.getNom();
+                        antiguitat = alumne.getAnyMatricula();
+                        adreca = adrecaTutor[0];
+                        UtilsExamenUF3.ImprimirEtiqueta(nom, antiguitat, adreca);
+                    }
+
+                } else if (alumne instanceof AlumneMajor) {
+                    nom = alumne.getNom();
+                    antiguitat = alumne.getAnyMatricula();
+                    System.out.print("Introdueix una adreça");
+                    adreca = scan.next();
+                    UtilsExamenUF3.ImprimirEtiqueta(nom, antiguitat, adreca);
+                }
+
+            } else {
+                System.out.println("No s'ha trobat cap alumne amb aquest DNI");
+            }
+
+        }
 
     }
 
